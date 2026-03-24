7413732d8bc4611e97fef9e705a724aaf7580571
polardb-dev-team <polardb-dev-team@awide.io>
2026-03-24 14:11:05 +0300

    build: remove redundant runtime dependencies
    Refs: N/A
    Cherry-picked-from: 16dd0b62d972e56604f71efd7acb2532e9ac5137

--- package/deb/build-deb.sh
+++ package/deb/build-deb.sh
@@ -42,6 +42,31 @@ EOF
     }
 }
 
+build_package() {
+    local package_type=$1
+    local version=$2
+    local release=$3
+
+    fpm -s dir -t "$package_type" -n polarfs -v "$version" --iteration "$release" \
+        --log info \
+        -d libboost-atomic1.74.0 \
+        -d libboost-context1.74.0 \
+        -d libboost-filesystem1.74.0 \
+        -d libboost-program-options1.74.0 \
+        -d libboost-regex1.74.0 \
+        -d libboost-system1.74.0 \
+        -d libboost-thread1.74.0 \
+        -d libdouble-conversion3 \
+        -d libfmt-dev \
+        -d libgoogle-glog-dev \
+        -d libgflags2.2 \
+        -d libaio1 \
+        -d python3 \
+        --force \
+        --description "Alibaba PolarFS distributed file system" \
+        -C install etc/ usr/ var/
+}
+
 main() {
     if [[ $# -gt 1 ]]; then
            usage
